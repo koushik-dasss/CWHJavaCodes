821d1e984c05c715741dea991e050347459421fc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-07 08:24:45 +0530

    Lecture-2 added

---
+++ CWHLectureCodes/Lecture2.java
@@ -0,0 +1,14 @@
+package CWHLectureCodes;
+
+public class Lecture2 {
+    public static void main(String[] args){
+        // add two number
+        // AddTwoNumber 
+        //(Pascal Naming Convention for classes)
+        // addTwoNumber 
+        //(camel Case Naming Convention for functions)
+
+    }        
+    
+    
+}
