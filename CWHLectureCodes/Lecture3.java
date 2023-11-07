4ecdd59bdfcd1f06383fdb99cf94d3d8dfa2d202
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-07 08:51:27 +0530

    Lecture-3 added

---
+++ CWHLectureCodes/Lecture3.java
@@ -0,0 +1,22 @@
+package CWHLectureCodes;
+
+public class Lecture3 {
+    public static void main(String[] args){
+        
+        /* No digit  in the beginning but can be used at any other place 
+        name is case sensitive
+        should not be a keyword
+        white space not allowed
+        _ and $ character are allowed  */ 
+
+        // JAVA is statically typed 
+        int a =10,b=20,c=30,sum=a+b+c;
+        System.out.println("The sum of three numbers is :"+ sum);
+
+
+        
+        
+    
+    }   
+    
+}
