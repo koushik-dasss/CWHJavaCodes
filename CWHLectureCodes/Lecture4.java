e4a7b5ded68e1d830a914c05a3d20febbeda42b6
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-07 21:37:25 +0530

    Lecture-4 added

---
+++ CWHLectureCodes/Lecture4.java
@@ -0,0 +1,34 @@
+package CWHLectureCodes;
+
+public class Lecture4 {
+    public static void main(String[] args){
+        int age1 = 34;
+        byte age2 = 34;
+        short age3 = 34;
+        long age4 = 34; // L/l not compulsory anymore
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        char ch ='A';
+        float f1 = 5.6f; // F/f compulsory
+        double d1 = 4.66; // D/d not compulsory
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        boolean a = true;
+        System.out.println(a);
+        String k = "Koushik";
+        System.out.println(k);
+        
+
+
+ 
+
+
+
+
+        
+    }
+    
+}
