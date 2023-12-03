61ee185245168b54919c9914b9502da08b94cc6a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 16:46:51 +0530

    Lecture -1 revised with theory

--- CWHLectureCodes/Lecture1.java
+++ CWHLectureCodes/Lecture1.java
@@ -1,8 +1,7 @@
 package CWHLectureCodes;
 public class Lecture1{
     public static void main(String[] args) {
-        System.out.println("Hello World ! ");
-        System.out.println("This is my first code .... lol just saving my codes for fun");
+        System.out.println("Hello World");
         
     }
 }
