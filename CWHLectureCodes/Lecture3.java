b6f30927e0975aa384ff208f3a8630091254fa07
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 23:15:35 +0530

    Lecture-3 added , rules for declaration of variables

--- CWHLectureCodes/Lecture3.java
+++ CWHLectureCodes/Lecture3.java
@@ -3,20 +3,38 @@ package CWHLectureCodes;
 public class Lecture3 {
     public static void main(String[] args){
         
-        /* No digit  in the beginning but can be used at any other place 
-        name is case sensitive
-        should not be a keyword
-        white space not allowed
-        _ and $ character are allowed  */ 
+        System.out.println("Hello World !");
+        /*Function is a group/block  of code that does a particular work
+         Class is a collection of similar type of functions/attributes
+         Objects are under  functions , functions are under classes
+         Class is a template through which objects are made
+         psvm -- main method  -- function
+         public -- access modifier
+         static - object is dissociated  from the function of classes
 
-        // JAVA is statically typed 
-        int a =10,b=20,c=30,sum=a+b+c;
-        System.out.println("The sum of three numbers is :"+ sum);
+
+         Naming conventions :- 1) PascalNamingConvention = Classes
+                               2) camelCaseNamingConvention = Functions
+
+        Normally written = addtwonumbers
+        AddTwoNumbers- Pascal Naming Convention (Class)
+        addTwoNumbers- camel Case Naming Convention (Function) */
+
+        // = Single line comment
+        /* = Multi line comment
 
 
-        
-        
-    
-    }   
-    
+        package = set of codes(unique) written for a particular purpose
+
+
+         */
+
+    }
 }
+
+
+
+        
+        
+    
+    
