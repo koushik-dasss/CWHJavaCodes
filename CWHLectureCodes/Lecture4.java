8b46277e7d365681be582bde59fc1efa4604773e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 23:20:24 +0530

    Lecture-4 added, literals and use of various data types in JAVA

--- CWHLectureCodes/Lecture4.java
+++ CWHLectureCodes/Lecture4.java
@@ -2,33 +2,33 @@ package CWHLectureCodes;
 
 public class Lecture4 {
     public static void main(String[] args){
-        int age1 = 34;
-        byte age2 = 34;
-        short age3 = 34;
-        long age4 = 34; // L/l not compulsory anymore
-        System.out.println(age1);
-        System.out.println(age2);
-        System.out.println(age3);
-        System.out.println(age4);
-        char ch ='A';
-        float f1 = 5.6f; // F/f compulsory
-        double d1 = 4.66; // D/d not compulsory
-        System.out.println(ch);
-        System.out.println(f1);
-        System.out.println(d1);
-        boolean a = true;
-        System.out.println(a);
-        String k = "Koushik";
-        System.out.println(k);
-        
+ // Literals = constant value which can be assigned to a variable
+ byte age1 = 34;
+ System.out.println(age1);
+ int age2 = 56;
+ System.out.println(age2);
+ long age3 = 344444444, age4 = 344444444L , age5 = 344444444l ; //(L/l ) not compulsory for modern compilers
+ System.out.println(age3);
+ System.out.println(age4);
+ System.out.println(age5);
+ short age6 = 3444;
+ System.out.println(age6);
+ char ch = 'A';
+ System.out.println(ch);
+ float f1 = 5.66f , f2 = 5.66F ;//(f/F) Compulsory
+ double d1 = 4.66 , d2 = 4.66D; //(D/d) Not compulsory
+ System.out.println(d1);
+ System.out.println(d2);
+ System.out.println(f1);
+ System.out.println(f2);
+ boolean b1 = true  , b2 = false ;
+ System.out.println(b1);
+ System.out.println(b2);
+ String name = "Koushik";
+ System.out.println(name);
 
 
- 
-
-
-
-
-        
     }
-    
 }
+
+        
