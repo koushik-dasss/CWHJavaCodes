1eda83539e32f34b33642b411a69eb7bc112fbfc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 16:48:06 +0530

    Lecture-2 revised , naming conventions for classes and fucntions , what are the components in a basic java code

--- CWHLectureCodes/Lecture2.java
+++ CWHLectureCodes/Lecture2.java
@@ -1,14 +1,32 @@
 package CWHLectureCodes;
 
 public class Lecture2 {
-    public static void main(String[] args){
-        // add two number
-        // AddTwoNumber 
-        //(Pascal Naming Convention for classes)
-        // addTwoNumber 
-        //(camel Case Naming Convention for functions)
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        /*Function is a group/block  of code that does a particular work
+         Class is a collection of similar type of functions/attributes
+         Objects are under  functions , functions are under classes
+         Class is a template through which objects are made
+         psvm -- main method  -- function
+         public -- access modifier
+         static - object is dissociated  from the function of classes
 
-    }        
-    
-    
+
+         Naming conventions :- 1) PascalNamingConvention = Classes
+                               2) camelCaseNamingConvention = Functions
+
+        Normally written = addtwonumbers
+        AddTwoNumbers- Pascal Naming Convention (Class)
+        addTwoNumbers- camel Case Naming Convention (Function) */
+
+        // = Single line comment
+        /* = Multi line comment
+
+
+        package = set of codes(unique) written for a particular purpose
+
+
+         */
+
+    }
 }
